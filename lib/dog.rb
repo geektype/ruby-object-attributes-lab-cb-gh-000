76d8b6fde1947b67095cb4acaee35add0ae79335
Abdullah Ahmed <abdullah@softcomnetworks.com>
2019-08-07 06:57:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,8 +16,8 @@ class Dog
   end
 end
 
-dog = Dog.new
-dog.name = "fido"
-puts dog.name
-dog.breed = "German"
-puts dog.breed
+# dog = Dog.new
+# dog.name = "fido"
+# puts dog.name
+# dog.breed = "German"
+# puts dog.breed
